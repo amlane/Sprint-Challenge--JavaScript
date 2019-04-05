3b0ea1f2080bc705fe959be20fe15501c93b8667
amlane <twelvexstring@gmail.com>
2019-04-05 10:42:11 -0500

    completed 2 of the array challenges using for loops

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,9 +70,13 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
+
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -81,6 +85,9 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i=0; i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
